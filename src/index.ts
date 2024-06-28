86683b04473ba124bec2f6001247402c4f20c636
datdh <datdh@omegatheme.com>
2024-06-28 18:31:50 +0700

    error handling

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,14 @@
-const name: string = 'Dư Thanh Được'
-console.log(name)
+import express from 'express'
+import { sum } from './utils'
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  const data: any = { a: 1, b: 2 }
+  const value = sum(data)
+  res.send(`Hello World!, ${value}`)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
