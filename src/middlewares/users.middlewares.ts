1225bc1e596c7d2d45206d5d2e3a1c103b2923ff
datdh <datdh@omegatheme.com>
2024-07-10 15:33:55 +0700

    error message

--- src/middlewares/users.middlewares.ts
+++ src/middlewares/users.middlewares.ts
@@ -1,62 +1,121 @@
 import { NextFunction, Request, Response } from 'express'
 import { checkSchema } from 'express-validator'
-import { ErrorWithStatus } from '~/models/Errors'
-import userServices from '~/services/users.services'
+import { UserMessages } from '~/constants/messages'
+import usersService from '~/services/users.services'
 import { validate } from '~/utils/validation'
 
 export const loginValidator = (req: Request, res: Response, next: NextFunction) => {
-  const { username, password } = req.body || {}
-  if (!username || !password) {
-    return res.status(400).json({ msg: 'Missing username or password' })
+  const { email, password } = req.body
+  if (!email || !password) {
+    return res.status(400).json({
+      error: 'Missing email or password'
+    })
   }
   next()
 }
 
 export const registerValidator = validate(
   checkSchema({
-    email: {
-      isEmail: {
-        bail: true
-      },
+    name: {
       notEmpty: {
-        bail: true
+        errorMessage: UserMessages.NAME_IS_REQUIRED
+      },
+      isString: {
+        errorMessage: UserMessages.NAME_MUST_BE_A_STRING
+      },
+      isLength: {
+        options: {
+          min: 1,
+          max: 100
+        },
+        errorMessage: UserMessages.NAME_LENGTH_MUST_BE_FROM_1_TO_100
+      },
+      trim: true
+    },
+    email: {
+      notEmpty: {
+        errorMessage: UserMessages.EMAIL_IS_REQUIRED
+      },
+      isEmail: {
+        errorMessage: UserMessages.EMAIL_IS_INVALID
       },
       trim: true,
       custom: {
         options: async (value) => {
-          const existingUser = await userServices.findOne(value)
-          if (existingUser) {
-            throw new ErrorWithStatus({ message: 'E-mail already in use', status: 401 })
+          const isExistEmail = await usersService.findOne(value)
+          if (isExistEmail) {
+            throw new Error(UserMessages.EMAIL_ALREADY_EXISTS)
           }
+          return true
         }
       }
     },
-    name: {
-      notEmpty: true,
-      isString: true,
-      isLength: {
-        options: { min: 3, max: 100 }
-      },
-      trim: true
-    },
     password: {
-      notEmpty: true,
+      notEmpty: {
+        errorMessage: UserMessages.PASSWORD_IS_REQUIRED
+      },
+      isString: {
+        errorMessage: UserMessages.PASSWORD_MUST_BE_A_STRING
+      },
       isLength: {
-        options: { min: 3, max: 20 }
+        options: {
+          min: 6,
+          max: 50
+        },
+        errorMessage: UserMessages.PASSWORD_LENGTH_MUST_BE_FROM_6_TO_50
+      },
+      isStrongPassword: {
+        options: {
+          minLength: 6,
+          minLowercase: 1,
+          minUppercase: 1,
+          minNumbers: 1,
+          minSymbols: 1
+        },
+        errorMessage: UserMessages.PASSWORD_MUST_BE_STRONG
       }
     },
-    confirmPassword: {
+    confirm_password: {
+      notEmpty: {
+        errorMessage: UserMessages.CONFIRM_PASSWORD_IS_REQUIRED
+      },
+      isString: {
+        errorMessage: UserMessages.CONFIRM_PASSWORD_MUST_BE_A_STRING
+      },
+      isLength: {
+        options: {
+          min: 6,
+          max: 50
+        },
+        errorMessage: UserMessages.CONFIRM_PASSWORD_LENGTH_MUST_BE_FROM_6_TO_50
+      },
+      isStrongPassword: {
+        options: {
+          minLength: 6,
+          minLowercase: 1,
+          minUppercase: 1,
+          minNumbers: 1,
+          minSymbols: 1
+        },
+        errorMessage: UserMessages.CONFIRM_PASSWORD_MUST_BE_STRONG
+      },
       custom: {
         options: (value, { req }) => {
           if (value !== req.body.password) {
-            throw new Error('Passwords do not match')
+            throw new Error(UserMessages.CONFIRM_PASSWORD_MUST_BE_THE_SAME_AS_PASSWORD)
           }
           return true
         }
       }
     },
     date_of_birth: {
-      notEmpty: true
+      isISO8601: {
+        options: {
+          strict: true,
+          strictSeparator: true
+        },
+        errorMessage: UserMessages.DATE_OF_BIRTH_MUST_BE_ISO8601
+      }
     }
   })
 )
